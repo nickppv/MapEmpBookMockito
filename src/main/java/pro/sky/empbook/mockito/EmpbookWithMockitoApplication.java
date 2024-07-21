fa998f8a8310e160ecafd722b2d43d47ce2165e0
pvnick <pvnick@yandex.ru>
2024-07-21 19:39:28 +0700

    Initial commit

---
+++ src/main/java/pro/sky/empbook/mockito/EmpbookWithMockitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.empbook.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpbookWithMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpbookWithMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/empbook/mockito/EmpbookWithMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.empbook.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpbookWithMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
